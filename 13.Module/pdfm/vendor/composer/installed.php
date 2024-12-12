991bbd2566d8f3b6dbda301ab388ee88c96e4132
Soykot Hosen <soykothosen1603028@gmail.com>
2024-12-12 20:01:17 +0600

    pdf maker added

---
+++ 13.Module/pdfm/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'soykot/pdfm',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '26db8b25924cf2ec49af76dd51792ec1b9c1873c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'soykot/pdfm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '26db8b25924cf2ec49af76dd51792ec1b9c1873c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.7.7',
+            'version' => '6.7.7.0',
+            'reference' => 'cfbc0028cc23f057f2baf9e73bdc238153c22086',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
